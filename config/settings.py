7e8c7b953b1e0f9a324b92680267ab684c5cf081
taa <TahaM8000@gmail.com>
2023-11-11 21:33:09 +0330

    update settings

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,8 @@
+from corsheaders.defaults import default_headers
 from pathlib import Path
-from datetime import timedelta
-from decouple import config
 import os
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-AUTH_USER_MODEL = 'accounts.User'
 
 
 deploy = True
@@ -17,48 +13,12 @@ if(deploy):
     password_email = os.getenv('EMAIL_HOST_PASSWORD', 'LIARA_URL is not set.')
     DEBUG = os.getenv('DEBUG', 'LIARA_URL is not set.')
     admin_url = os.getenv('ADMIN', 'LIARA_URL is not set.')
-else:
-    # local
-    SECRET_KEY = config('SECRET_KEY')
-    hotel_email = config('EMAIL_HOST')
-    password_email = config('EMAIL_HOST_PASSWORD')
-    admin_url = config('ADMIN')
-    DEBUG = True
 
 
-#React
 ALLOWED_HOSTS = ["*"]
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+AUTH_USER_MODEL = 'accounts.User'
+CORS_ORIGIN_ALLOW_ALL=True
 
-CORS_ALLOW_METHODS = [
-'DELETE',
-'GET',
-'OPTIONS',
-'PATCH',
-'POST',
-'PUT',
-]
-
-CORS_ALLOW_HEADERS = [
-'accept',
-'accept-encoding',
-'authorization',
-'content-type',
-'dnt',
-'origin',
-'user-agent',
-'x-csrftoken',
-'x-requested-with',
-]
-
-CORS_ORIGIN_ALLOW_ALL = True
-
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:3000',  # for localhost (REACT Default)
-#     'http://localhost:5173',
-#     'http://192.168.10.45:5173',  # for network
-# )
 
 # Application definition
 
@@ -69,36 +29,52 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #news:
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
-    'drf_yasg'
 ]
+
 
 INSTALLED_APPS += [
-    'accounts',
+    'accounts.apps.AccountsConfig',
 ]
+
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://domain.com",
+    "https://api.domain.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:9000"
+]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',  # for localhost (REACT Default)
+    'http://192.168.10.45:3000',  # for network
+)
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # +++
+    'django.middleware.common.CommonMiddleware',  # +++
     'django.middleware.security.SecurityMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-
-
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    
+    
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
-
 ROOT_URLCONF = 'config.urls'
+
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'react/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,28 +87,18 @@ TEMPLATES = [
     },
 ]
 
+
+
+# LOGIN_REDIRECT_URL = "main"
+LOGIN_REDIRECT_URL = "react"
+LOGOUT_REDIRECT_URL = ""
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
-# if(deploy):
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv('database_name', 'LIARA_URL is not set.'),
-#             'USER': os.getenv('database_username', 'LIARA_URL is not set.'),
-#             'PASSWORD': os.getenv('password', 'LIARA_URL is not set.'),
-#             'HOST': os.getenv('database_hostname_or_ip', 'LIARA_URL is not set.'),
-#             'PORT': os.getenv('database_port', 'LIARA_URL is not set.'),
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# SQLITE
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -140,8 +106,9 @@ DATABASES = {
     }
 }
 
+
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -160,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -168,75 +135,36 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+# STATIC_ROOT =  os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'react/build/'),
+    # os.path.join(BASE_DIR, 'media/'),
+    # os.path.join(BASE_DIR, 'static/'),
+]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
-
-
-
-# REST FRAMEWORK
-JWT_SECRET_KEY = 'er9238fnwi7fy2d3n3f239r87dcjknwq0e92'
-JWT_ALGORITHM = 'HS256'
-
+# Rest FrameWork
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+    ],
+    # 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+
 }
-
-# Django project settings.py
-
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
-    "UPDATE_LAST_LOGIN": False,
-
-    "ALGORITHM": "HS256",
-    "VERIFYING_KEY": "",
-    "AUDIENCE": None,
-    "ISSUER": None,
-    "JSON_ENCODER": None,
-    "JWK_URL": None,
-    "LEEWAY": 0,
-
-    "AUTH_HEADER_TYPES": ("Bearer",),
-    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
-    "USER_ID_FIELD": "id",
-    "USER_ID_CLAIM": "user_id",
-    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
-
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
-    "TOKEN_TYPE_CLAIM": "token_type",
-    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
-
-    "JTI_CLAIM": "jti",
-
-    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
-    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
-    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
-
-    "TOKEN_OBTAIN_SERIALIZER": "accounts.serializers.EnhancedTokenObtainPairSerializer",
-    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
-    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
-    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
-    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
-    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
-}
-
-
-CORS_ALLOW_ALL_ORIGINS = True

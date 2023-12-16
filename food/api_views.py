8162a815fbf8933c1e0558c3dfb51400fa56f254
taa <TahaM8000@gmail.com>
2023-12-16 05:04:08 +0330

    Adding comments and explanations to views and models

--- food/api_views.py
+++ food/api_views.py
@@ -7,12 +7,30 @@ from .serializers import *
 from rest_framework.permissions import IsAuthenticated
 from .models import Food
 # -------------------------------------------------------------------------------------------------------------------------------
+"""
+    Food reservation and food CRUD are coded in this app.
+    api's in api_views.py :
+
+    1- FoodCreateAPIView --> create a food
+    2- FoodAllListAPIView --> List of all foods
+    3- FoodDetailView  --> Getting the information of a food with ID
+    4- FoodDeleteView --> Remove a food with an ID
+    5- FoodUpdateView --> Update food information with ID
+
+    6- ReservationRoomAPIView --> Reserve a food
+    7- ReservationAllListAPIView  --> List of all Reserves
+    8- UserFoodPaymentAPIView --> Report the amount of the user's food reservations
+    8- FoodImageUpdateView --> Updating food photos
+
+"""
+# -------------------------------------------------------------------------------------------------------------------------------
 class FoodCreateAPIView(APIView):
     """
+        create a food
         {
             "price": 2000,
-            "name": "Joje",
-            "meal": "m",
+            "name": "Pizza",
+            "meal": "n", --> night
             "type": "text",
             "count": 200
         }
@@ -22,25 +40,29 @@ class FoodCreateAPIView(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'room created.','data' : serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({'message': 'food created.','data' : serializer.data}, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 # -------------------------------------------------------------------------------------------------------------------------------
 class FoodAllListAPIView(ListAPIView):
+    """List of all foods"""
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
 # -------------------------------------------------------------------------------------------------------------------------------
 class FoodDetailView(RetrieveAPIView):
+    """Getting the information of a food with ID(domain.com/..../pk/)"""
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     lookup_field = 'pk'
 # -------------------------------------------------------------------------------------------------------------------------------
 class FoodDeleteView(DestroyAPIView):
+    """Remove a food with an ID(domain.com/..../pk/)"""
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     lookup_field = 'pk'
 # -------------------------------------------------------------------------------------------------------------------------------
 class FoodUpdateView(UpdateAPIView):
+    """Update food information with ID(domain.com/..../pk/)"""
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     lookup_field = 'pk'
@@ -48,6 +70,7 @@ class FoodUpdateView(UpdateAPIView):
 class ReservationRoomAPIView(APIView):
     permission_classes = [IsAuthenticated]
     """
+        Reserve a food
         {
             "food_id": 2
         }
@@ -77,14 +100,13 @@ class ReservationRoomAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 # -------------------------------------------------------------------------------------------------------------------------------
 class ReservationAllListAPIView(ListAPIView):
+    """List of all Reserves"""
     queryset =  FoodReservation.objects.all()
     serializer_class =  FoodReservationListSerializer
 # -------------------------------------------------------------------------------------------------------------------------------
 class UserFoodPaymentAPIView(APIView):
     permission_classes = [IsAuthenticated]
-    """
-        get food payment for user
-    """
+    """get food payment for user"""
     def get(self, request):
         payments_objects = request.user.food_reservations.all()
         payments = FoodReservationListSerializer(payments_objects,many = True)
@@ -92,6 +114,7 @@ class UserFoodPaymentAPIView(APIView):
         return Response({'payments': payments.data}, status=status.HTTP_200_OK)
 # -------------------------------------------------------------------------------------------------------------------------------
 class FoodImageUpdateView(APIView):
+    """Updating food photos"""
     def put(self, request,pk):
         try:
             user = Food.objects.get(pk=pk)
--- food/models.py
+++ food/models.py
@@ -3,10 +3,7 @@ from accounts.models import User
 from config.utils import jalali_create,persion_converter_number
 # ----------------------------------------------------------------------------------------------------------------------------
 class Food(models.Model):
-    """
-        Foods Model.
-
-    """
+    """The Food model defines a database table for storing food items."""
     meal_choice = (
         ('m','صبحانه'),
         ('d','ناهار'),
@@ -15,7 +12,6 @@ class Food(models.Model):
     price = models.IntegerField()
     name = models.CharField(max_length=100)
     meal = models.CharField(max_length=1,choices=meal_choice)
-    # reserve = models.ManyToManyField(User)
     type = models.CharField(max_length=100)
     reserved = models.IntegerField(default=0)
     count = models.IntegerField(default=0)
@@ -28,9 +24,11 @@ class Food(models.Model):
         return str(self.name) + "-"
 
     def remain(self):
+        """The number of dishes that can be reserved according to the reservations made."""
         return self.count - self.reserved
 
     def shamsi_date(self):
+        """Date of serving food in Shamsi"""
         temp = jalali_create(self.day)
         return f"{temp[0]}-{temp[1]}-{temp[2]}"
 
@@ -38,6 +36,7 @@ class Food(models.Model):
 
 # ----------------------------------------------------------------------------------------------------------------------------
 class FoodReservation(models.Model):
+    """The FoodReservation model represents a reservation made by a user for a specific food item."""
     food = models.ForeignKey(Food,on_delete=models.CASCADE,related_name="reservations")
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="food_reservations")
     created = models.DateTimeField(auto_now_add=True)
@@ -51,9 +50,11 @@ class FoodReservation(models.Model):
         return self.food.price
 
     def remaining(self):
+        """The amount of unpaid food"""
         return self.price() - self.been_paid
 
     def shamsi_date(self):
+        """Date of serving food in Shamsi"""
         temp = jalali_create(self.created)
         return f"{temp[0]}-{temp[1]}-{temp[2]}"
 

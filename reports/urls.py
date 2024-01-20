c8ca9e2a687dfcc5b916a6d86ba57a318e541414
taa <TahaM8000@gmail.com>
2024-01-21 02:00:40 +0330

    chart all report

--- reports/urls.py
+++ reports/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('reports/food/', FoodSalesReportAPIView.as_view(), name='food-sales-report'),
     path('reports/room/', RoomReservationReportAPIView.as_view(), name='room-reservation-report'),
     path('reports/all/', AllReportAPIView.as_view(), name='all-report'),
+    path('charts/year/', YearChartAPIView.as_view(), name='charts-year'),
+    path('charts/month/', MonthChartAPIView.as_view(), name='charts-month'),
+    path('charts/day/', DayChartAPIView.as_view(), name='charts-day'),
 
     path('payment/', PayMoneyAPIView.as_view(), name='pay'),
     path('verify/', VerifyAPIView.as_view(), name='verify'),
